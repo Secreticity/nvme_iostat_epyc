8d5ce597e9fb76353d17b889e7251c4812ddb409
root <root@epyc01.kisti.re.kr>
2020-06-01 16:40:40 +0900

    200601-1640

--- aresult_to_csv_ior_write.py
+++ aresult_to_csv_ior_write.py
@@ -4,11 +4,11 @@ import pandas as pd
 #---------------- Save Settings ------------------
 
 # File name to save DataFrame into csv
-save_name = "out_mod4epyc2_ior_w"
+save_name = "out_mod16epyc2_ior_w"
 #save_name = "org_npb"
 
 # File name of the iostat/throughput result
-out_file = "out_mod4epyc2"
+out_file = "out_mod16epyc2"
 #-------------------------------------------------
 '''
 state = "INIT"
--- profiling.sh
+++ profiling.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #------------ SETTING VARIABLES
-filepath='/home/kau/jwbang/200320/out_mod4epyc2.txt'
+filepath='/home/kau/jwbang/200320/out_mod16epyc2.txt'
 
-annot="out_mod4epyc2"
+annot="out_mod16epyc2"
 #path="/home/kau/jwbang/linux-5.2.8_org/mymodule/mymodule.ko"
 
 #annot="MODIFIED2"
